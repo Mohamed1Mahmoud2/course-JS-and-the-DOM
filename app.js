397f7a0846fd8bc27c597e4530683d662fe08036
Mohamed Mahmoud <103222555+Mohamed1Mahmoud2@users.noreply.github.com>
2022-10-10 05:29:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-const nanodegreeCard = document.querySelector('.card');
+const communityHeeding = dovument.querySelector("h6.text-center");
+
+communityHeeding.style.fontSize = '2em';
+communityHeeding.style.color = "purplr";
