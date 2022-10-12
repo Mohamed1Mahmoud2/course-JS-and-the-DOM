5bd46bd8f677846dd977815fc802b5321af22003
Mohamed Mahmoud <103222555+Mohamed1Mahmoud2@users.noreply.github.com>
2022-10-13 01:37:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const communityHeeding = dovument.querySelector("h6.text-center");
+const communityHeeding = document.querySelector("h6.text-center");
 
 communityHeeding.style.fontSize = '2em';
 communityHeeding.style.color = "purplr";
+
+document.addEventListener("click", function() {
+  console.log('be clicked.!');
+});
