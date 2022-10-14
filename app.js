4f460ef5f4161d6fea1e5776f8a57f1641faadf1
Mohamed Mahmoud <103222555+Mohamed1Mahmoud2@users.noreply.github.com>
2022-10-14 03:11:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,3 +6,18 @@ communityHeeding.style.color = "purplr";
 document.addEventListener("click", function() {
   console.log('be clicked.!');
 });
+
+
+const myCustomDiv = document.createElement("div");
+
+for (let i = 1; i <= 200; i++) {
+    const newElement = document.createElement("p");
+
+    newElement.textContent = 'This is Paragraph Number' + i;
+    newElement.addEventListener('click', function respondToTheClick() {
+        console.log('A P was clicked.');
+    });
+    myCustomDiv.appendChild(newElement);
+
+}
+document.body.appendChild(myCustomDiv);
